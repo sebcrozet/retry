d87e0e823e59fa770fb21b104973dce63b27565f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-04-02 03:00:58 -0700

    v0.4.0

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['retry'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='retry/struct.RetryError.html' title='retry::RetryError'>RetryError</a>",];
+implementors['retry'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='retry/struct.RetryError.html' title='retry::RetryError'>RetryError</a>",];implementors['retry'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='retry/struct.RetryError.html' title='retry::RetryError'>RetryError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
